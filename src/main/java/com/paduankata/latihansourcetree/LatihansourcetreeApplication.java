93c5ba8f3be66c5269c288d8a9560affa1a8d807
dompaksi <dompak.sinambela@gmail.com>
2020-06-08 15:45:56 +0700

    Initial commit

---
+++ src/main/java/com/paduankata/latihansourcetree/LatihansourcetreeApplication.java
@@ -0,0 +1,13 @@
+package com.paduankata.latihansourcetree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihansourcetreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatihansourcetreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paduankata/latihansourcetree/LatihansourcetreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paduankata.latihansourcetree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihansourcetreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
